193bc98b5b46887c64e255e7669c534adb0810dc
Benjamin West <mrgenixus@gmail.com>
2016-06-21 22:18:43 -0600

    Add some initial reducers, specs

---
+++ spec/reducers/todo-spec.js
@@ -0,0 +1,36 @@
+import todo from 'reducers/todo';
+
+describe('todo reducer', function() {
+  it('is a function', function() {
+    expect(todo).to.be.a('function');
+  });
+
+  it('should return a new todo', function() {
+    const stateBefore = undefined;
+    const action = {
+      type: 'ADD_TODO',
+      id: 0,
+      text: 'Learn Redux'
+    };
+    const stateAfter = {
+      id: 0,
+      text: 'Learn Redux',
+      completed: false
+    };
+
+    deepFreeze(action);
+
+    expect(todo(stateBefore, action)).to.deep.equal(stateAfter);
+  });
+
+  it('toggles a todo\'s completed field', function() {
+    const stateBefore = { id: 1, text: 'Go shopping', completed: false };
+    const action = { id: 1, type: 'TOGGLE_TODO' };
+    const stateAfter = { id: 1, text: 'Go shopping', completed: true };
+
+    deepFreeze(stateBefore);
+    deepFreeze(action);
+
+    expect(todo(stateAfter, action)).to.deep.equal(stateAfter);
+  });
+});
---
+++ src/reducers/todos.js
@@ -0,0 +1,13 @@
+import todo from './todo';
+
+export default function(state = [], action) {
+  switch(action.type) {
+    case 'ADD_TODO':
+      return [...state, todo(undefined, action)];
+
+    case 'TOGGLE_TODO':
+      return state.map(t => todo(t, action));
+    default:
+      return state;
+  }
+}
