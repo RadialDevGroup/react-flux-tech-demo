f3b3c36a13b3ba745d0b21df58ba2eccc1a8be16
Benjamin West <mrgenixus@gmail.com>
2016-06-21 22:16:05 -0600

    Add test suite

---
+++ spec/index.js
@@ -0,0 +1,7 @@
+import './specHelper'
+
+const context = require.context('..', true, /.+-spec\.js$/)
+
+context.keys().forEach(context)
+
+module.exports = context
---
+++ webpack.config-test.js
@@ -0,0 +1,39 @@
+var webpack = require('webpack');
+var path = require('path');
+var nodeExternals = require('webpack-node-externals');
+
+module.exports = {
+  output: {
+    // sourcemap support for IntelliJ/Webstorm
+    devtoolModuleFilenameTemplate: '[absolute-resource-path]',
+    devtoolFallbackModuleFilenameTemplate: '[absolute-resource-path]?[hash]'
+  },
+  module: {
+    loaders: [
+      {
+        test: /\.js$/,
+        exclude: /node_modules/,
+        loader: "babel-loader",
+        query: {
+          presets: ['es2015', 'react'],
+          plugins: ['transform-react-jsx', 'react-autorequire']
+        }
+      }
+    ]
+  },
+    plugins: [
+    //provides variables by default to all modules
+    new webpack.ProvidePlugin({
+      'deepFreeze': 'deep-freeze',
+      'React': 'react',
+      'ReactDom': 'react-dom',
+      'Redux': 'redux'
+    }),
+  ],
+  resolve: {
+    root: [path.resolve(__dirname, 'src'), path.resolve(__dirname, 'node_modules')]
+  },
+  target: 'node', // in order to ignore built-in modules like path, fs, etc.
+  externals: [nodeExternals()], // in order to ignore all modules in node_modules folder
+  devtool: "cheap-module-source-map" // faster than 'source-map'
+};
